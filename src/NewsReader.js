37c5f0faf30e8b7d38ab774ef0db5e33daee0a97
Tina Davis <christina.davis@libertymutual.com>
2024-04-30 18:29:27 -0400

    SavedQueries are now retained after refresh

--- src/NewsReader.js
+++ src/NewsReader.js
@@ -10,12 +10,45 @@ export function NewsReader() {
   const [data, setData] = useState(exampleData);   // current data returned from newsapi
   const [queryFormObject, setQueryFormObject] = useState({ ...exampleQuery });
   const urlNews="/news";
+  const urlQueries = "/queries";
   const [savedQueries, setSavedQueries] = useState([{ ...exampleQuery }]);
 
   useEffect(() => {
     getNews(query);
   }, [query])
 
+  useEffect(() => {getQueryList();}, [])
+
+  async function getQueryList() {
+    try {
+      const response = await fetch(urlQueries);
+        if (response.ok) {
+        const data = await response.json();
+        console.log("savedQueries has been retrieved: ");
+        setSavedQueries(data);
+      }
+      } catch (error) {
+      console.error('Error fetching news:', error);
+    }
+  } 
+  
+  async function saveQueryList(savedQueries) {
+    try {
+      const response = await fetch(urlQueries, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(savedQueries),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      console.log("savedQueries array has been persisted:");
+    } catch (error) {
+      console.error('Error fetching news:', error);
+    }
+  }
+  
   function onSavedQuerySelect(selectedQuery) {
     setQueryFormObject(selectedQuery);
     setQuery(selectedQuery);
@@ -30,6 +63,7 @@ export function NewsReader() {
       }
     }
     console.log(JSON.stringify(newSavedQueries));
+    saveQueryList(newSavedQueries);
     setSavedQueries(newSavedQueries);
     setQuery(queryObject);
   }
