f4dc2a943aa47251dd8b17be180fe59e6dda1167
Tina Davis <christina.davis@libertymutual.com>
2024-05-02 15:24:04 -0400

    display query details in article list

--- src/Articles.js
+++ src/Articles.js
@@ -3,12 +3,40 @@ export function Articles(params) {
     let queryName = (params.query.queryName)?params.query.queryName:"na";
     let articleCount = (params.data.totalResults)?params.data.totalResults:0;
     let query = (params.query.q)?params.query.q:[];
+    let lang = (params.query.language)?params.query.language:"";
+    let size = (params.query.pageSize)?params.query.pageSize:0;
+
+    // https://stackoverflow.com/questions/1199352/smart-way-to-truncate-long-strings
+    function truncate( str, n, useWordBoundary ){
+      if (str.length <= n) { return str; }
+      const subString = str.slice(0, n-1); // the original check
+      return (useWordBoundary 
+        ? subString.slice(0, subString.lastIndexOf(" ")) 
+        : subString) + "&hellip;";
+    };
+
+    function onDetailsClick(event) {
+      event.preventDefault();
+      const detailsList = document.getElementById('detailsList');
+
+      if (detailsList.style.display === 'none') {
+        detailsList.style.display = 'block';
+      } else {
+        detailsList.style.display = 'none';
+      }
+    };
+
     return (
       <div>
         Query: {queryName}
         <br/>Count: {articleCount}<br/>
-        <div>Details: 
-          <li>Search text: {query}</li>
+        <div>
+          <input type="button" value="Details: " onClick={onDetailsClick} /> 
+          <ul id="detailsList">
+            <li>Search text: {query}</li>
+            <li>Language: {lang}</li>
+            <li>Page Limit: {size}</li>
+          </ul>
         </div>
         <ol >{
             articles.map((item, idx) => {
@@ -17,7 +45,7 @@ export function Articles(params) {
                   if(item.title === "[Removed]"){
                     return (<li key={idx} >Was Removed</li>);
                   }
-                  let trimTitle = item.title.substring(0,30);
+                  let trimTitle = truncate(item.title, 90);
                   return (<li key={idx}>{trimTitle}<a href={item.url} target="_blank" rel="noreferrer" >&nbsp;Link</a></li>);    
                 }else{
                   return (<li key={idx}>No Title</li>);
